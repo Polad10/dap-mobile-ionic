830ad7b43d02cd38418db537df128a59636f263f
Amelia Kezia Gunawan <amelia.kezia.gunawan@gmail.com>
2021-06-18 17:04:07 +0200

    UI design for patient details

--- src/views/details/patient_details.vue
+++ src/views/details/patient_details.vue
@@ -32,7 +32,7 @@
       <div class="menu_text">Treatments</div>
     </div>
     <div class="menu_item item" data-tab="appointments">
-      <i class="large tablets icon mr-0"></i>
+      <i class="large calendar check outline icon"></i>
       <div class="menu_text">Appointments</div>
     </div>
   </div>
@@ -91,7 +91,11 @@
         </ion-item>
       </ion-list>
     </div> -->
-    <div class="ui large divided selection tab list" data-tab="treatments">
+    <div
+      id="treatment_list"
+      class="ui large divided selection tab list"
+      data-tab="treatments"
+    >
       <div class="item">
         <div class="list_icon right floated content">
           <i class="angle right icon"></i>
@@ -124,6 +128,77 @@
       <p>appointments</p>
     </div>
   </div>
+
+  <div class="ui overlay fullscreen modal">
+    <div class="header">Header</div>
+    <div class="content">
+      <div class="ui vertical segment pt-0 mt-10 pb-20">
+        <h3 class="ui header">
+          Patient Name
+          <div class="sub header">
+            Diagnosis Diagnosis Diagnosis Diagnosis Diagnosis Diagnosis
+            Diagnosis Diagnosis
+          </div>
+        </h3>
+        <div id="treatment_overview" class="ui two column grid mt-20">
+          <div class="column">
+            <h4 class="ui header">
+              <i class="blue calendar alternate outline icon"></i>
+              <div class="content">
+                17 Feb 2020
+                <div class="sub header">Start Date</div>
+              </div>
+            </h4>
+          </div>
+          <div class="column">
+            <h4 class="ui header">
+              <i class="teal dollar sign icon"></i>
+              <div class="content">
+                12.500
+                <div class="sub header">Price left to be paid</div>
+              </div>
+            </h4>
+          </div>
+          <div class="column">
+            <h4 class="ui header">
+              <i class="blue calendar alternate icon"></i>
+              <div class="content">
+                20 Feb 2020
+                <div class="sub header">End Date</div>
+              </div>
+            </h4>
+          </div>
+          <div class="column">
+            <h4 class="ui header">
+              <i class="teal hospital outline icon"></i>
+              <div class="content">
+                Ongoing
+                <div class="sub header">Treatment Status</div>
+              </div>
+            </h4>
+          </div>
+        </div>
+      </div>
+      <div class="ui vertical segment mt-5">
+        <h4 class="ui header">
+          Extra Info
+        </h4>
+        <div class="treatment_container">
+          <div class="ui extra_info_segment segment">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in
+            porttitor ante. Pellentesque finibus lorem eget tempor fringilla.
+            Maecenas facilisis, arcu at ultrices ultrices, libero nisl posuere
+            urna, at tempor metus mauris faucibus erat.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="actions">
+      <div class="ui approve button">Approve</div>
+      <div class="ui button">Neutral</div>
+      <div class="ui cancel button">Cancel</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -133,6 +208,10 @@ export default defineComponent({
   mounted() {
     setTimeout(() => {
       window.$(".menu .item").tab();
+      // window.$(".overlay.fullscreen.modal").modal("show");
+      window
+        .$(".overlay.fullscreen.modal")
+        .modal("attach events", "#treatment_list .item", "show");
     }, 400);
   },
 });
